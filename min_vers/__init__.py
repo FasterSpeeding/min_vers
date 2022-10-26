338ca23b200d1698d1c8b6bd583cf08dd0f7326a
Lucina <lucina@lmbyrne.dev>
2022-10-26 05:30:54 +0100

    Update __init__.py

--- min_vers/__init__.py
+++ min_vers/__init__.py
@@ -38,7 +38,7 @@ import pathlib
 import typing
 
 import packaging.requirements
-import pkginfo  # pyright: ignore [ reportMissingTypeStubs]
+import pkginfo  # pyright: ignore [ reportMissingTypeStubs ]
 
 if typing.TYPE_CHECKING:
     import shippinglabel.requirements
